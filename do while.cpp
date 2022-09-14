9ab680697bd550ad88d26e62e883511e41740f59
SahilBillaiya <91463791+SahilBillaiya@users.noreply.github.com>
2022-09-14 23:17:56 +0530

    Create do while.cpp

---
+++ do while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    do{
+        cout<<n;
+        cin>>n;
+        }
+        while(n>0);
+        {
+            cout<<n;
+    }
+    return(0);
+}
