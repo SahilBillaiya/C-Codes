3ef9454b6202180840967f46e1afc1d0fe4a362c
SahilBillaiya <91463791+SahilBillaiya@users.noreply.github.com>
2022-10-03 16:35:02 +0530

    Create size of array .cpp

---
+++ size of array .cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int size) {
+
+    cout<< " printing the array " << endl;
+    for(int i =0; i<size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout<< " printing DONE " << endl;
+}
+
+int main() {
+    int number[4];
+    cout << "Value at 3 index " << number[3] << endl;
+
+    int shiv[3] = {5, 7, 11};
+    cout << "Value at 2 index " << shiv[2] << endl;
+
+    int third[15] = {2,7};
+    int n= 15;
+    printArray(third,n);
+
+    int fourth[10] = {0};
+    n = 10;
+    printArray(fourth,4);
+
+    int fifth[10] = {1};
+    n = 10;
+    printArray(fifth,n);
+    
+    int fifthsize = sizeof(fifth)/sizeof(int);
+    cout<<"sizeof(fifth)"<<fifthsize<<endl;
+
+    char ch[4] = {'a','b','s','k'};
+    cout << ch[2] << endl;
+    
+     cout<< " printing the array " << endl;
+    for(int i =0; i<4; i++) {
+        cout << ch[i] << " ";
+    }
+    cout<< " printing DONE " << endl;
+    double firstdouble[5];
+    float firstfloat[6];
+    bool firstbool[9];
+
+}
